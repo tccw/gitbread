2fced546fbd4ee3d8a56d437bb07e2ade78a7fc2
tccw <thomas.cowan@gmail.com>
2020-02-29 11:06:38 -0800

    PHASE 3: Fixed HashCodeMaker (order of getter calls is maintained) and wrote jUnit tests.

--- src/main/model/HashCodeMaker.java
+++ src/main/model/HashCodeMaker.java
@@ -1,80 +1,48 @@
 package model;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HashCodeMaker {
 
     //method take from here: http://oliviertech.com/java/generate-SHA1-hash-from-a-String/
+    //EFFECTS: calculates a SHA-1 hashcode for version control
     public static String sha1(Recipe recipe) throws NoSuchAlgorithmException {
-        java.lang.String value = "";
-        try {
-            value = buildString(recipe);
-//            System.out.println(value);
-        } catch (InvocationTargetException e) {
-            System.out.println("InvocationTargetException during hash generation");
-        } catch (IllegalAccessException e) {
-            System.out.println("IllegalAccessException during hash generation");
-        }
+        String value = buildString(recipe);
         MessageDigest digest = MessageDigest.getInstance("SHA-1");
         digest.reset();
         digest.update(value.getBytes());
         return String.format("%040x", new BigInteger(1, digest.digest()));
     }
 
-
-    //MODIFIES:
-    //EFFECTS:
     // help iterating through specific methods here: https://stackoverflow.com/questions/11224217/
-    //TODO: determine why this is creating a different SHA-1 code every time it runs.
-    public static String buildString(Recipe recipe) throws InvocationTargetException, IllegalAccessException {
+    // the stackoverflow link uses getMethods() which does not return the methods in any particular order
+    //REQUIRES:
+    //EFFECTS: generates a string from a recipe to be used to calculate an SHA-1 hashcode for version control. For the
+    // purposes of a breadRecipe, only changes in the Baker's Percentages, the instructions, cooking vessel,
+    // or cook/prep time and cooking temperature are considered version changes.
+    public static String buildString(Recipe recipe) {
         StringBuilder string = new StringBuilder();
         string.append(recipe.getCookTemp());
         string.append(recipe.getCookTime());
         string.append(recipe.getPrepTime());
         string.append(recipe.getInstructions());
         if (recipe.getClass() == BreadRecipe.class) {
-            for (Method m : recipe.getClass().getMethods()) {
-                if (m.getName().startsWith("get")
-                        && (m.getName().contains("Fraction") || (m.getName().contains("CookingVessel")))) {
-                    string.append(m.invoke(recipe));
-                }
-            }
+            string.append(breadBuilder((BreadRecipe) (recipe)));
         }
-        String result = string.toString();
-        return result;
+        return string.toString();
     }
 
-    public static byte[] buildStringBytes(Recipe recipe) throws InvocationTargetException, IllegalAccessException {
-        StringBuilder string = new StringBuilder("UTF-8");
-        string.append(recipe.getCookTemp());
-        string.append(recipe.getCookTime());
-        string.append(recipe.getPrepTime());
-        string.append(recipe.getInstructions());
-
-        /*
-        Returns an array containing Method objects reflecting all the public member methods of the class or interface
-        represented by this Class object, including those declared by the class or interface and those inherited from
-        superclasses and superinterfaces. Array classes return all the (public) member methods inherited from the
-        Object class. **The elements in the array returned are not sorted and are not in any particular order.**
-        This method returns an array of length 0 if this Class object represents a class or interface that has no
-        public member methods, or if this Class object represents a primitive type or void.
-         */
-        //TODO: fix this so that the order is maintained
-
-        if (recipe.getClass() == BreadRecipe.class) {
-            for (Method m : recipe.getClass().getMethods()) {
-                if (m.getName().startsWith("get")
-                        && (m.getName().contains("Fraction") || (m.getName().contains("Vessel")))) {
-                    string.append(m.invoke(recipe));
-                }
-            }
-        }
-        return string.toString().getBytes();
+    private static String breadBuilder(BreadRecipe r) {
+        String result = r.getCookingVessel()
+                + r.getFlourFraction()
+                + r.getWaterFraction()
+                + r.getSaltFraction()
+                + r.getSugarFraction()
+                + r.getFatFraction()
+                + r.getYeastFraction();
+        return result;
     }
 }
 
--- src/test/model/TestHashCodeMaker.java
+++ src/test/model/TestHashCodeMaker.java
@@ -12,39 +12,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class TestHashCodeMaker {
-    String expected;
+    String stringExpected;
+    String sha1Expected;
     BreadRecipe testRecipe;
 
     @BeforeEach
     void setUp() {
         testRecipe = new BreadRecipe(1000);
-        expected = "425301351. Mix all ingredients " +
+        stringExpected = "425301351. Mix all ingredients " +
                 "2. Knead dough until smooth " +
                 "3. Let rise in oiled bowl for 1 hour " +
                 "4. Knock back, shape, and let rise for 45 minutes on baking pan lightly covered " +
-                "5. Bake 30 minutes at 425Fpan0.0061.00.020.660.00.0";
+                "5. Bake 30 minutes at 425Fpan1.00.660.020.00.00.006";
+        sha1Expected = "a23257d18ef6a34bb12cac1c1b808b3d912e5e1d";
     }
 
     @Test
     void TestSha1() {
         try {
             String sha1 = HashCodeMaker.sha1(testRecipe);
-            String sha1Expected = sha1Helper(expected);
+            String testClaculatedSha1 = sha1Helper(stringExpected);
+            assertEquals(testClaculatedSha1, sha1);
             assertEquals(sha1Expected, sha1);
         } catch (NoSuchAlgorithmException e) {
             fail();
         }
-
     }
 
     @Test
     void TestBuildString() {
-        try {
-            assertEquals(expected, HashCodeMaker.buildString(testRecipe));
-        } catch (InvocationTargetException | IllegalAccessException e) {
-            fail();
-            e.printStackTrace();
-        }
+        assertEquals(stringExpected, HashCodeMaker.buildString(testRecipe));
     }
 
     private String sha1Helper(String value) {
